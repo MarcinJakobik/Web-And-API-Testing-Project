200a1a3df2718b65f667909c8c975feb1b0937b7
Ed Bradbury <edwardsbradbury@gmail.com>
2023-04-18 11:35:51 +0100

    Written test for User Story 10, TC1

--- src/test/java/com/sparta/badgerBytes/testFramework/model/dto/SuperDTO.java
+++ src/test/java/com/sparta/badgerBytes/testFramework/model/dto/SuperDTO.java
@@ -5,8 +5,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class SuperDTO {
 
     @JsonProperty("responseCode")
-    private String responseCode;
+    private int responseCode;
 
     @JsonProperty("message")
     private String message;
+
+    public int getResponseCodes() {
+        return responseCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
