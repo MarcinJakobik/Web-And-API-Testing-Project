fbb80f9a349f3c5ce0f98ac5fb1938f16bc2a80d
Reggie Howard <92373993+Reg-Howard@users.noreply.github.com>
2023-04-18 10:15:12 +0100

    Test Setup Files
    Simple test layout so everyone is working from the same page.

---
+++ src/test/java/com/sparta/badgerBytes/tests/ProductListDTOTests.java
@@ -0,0 +1,20 @@
+package com.sparta.badgerBytes.tests;
+
+import org.junit.jupiter.api.BeforeAll;
+import com.sparta.badgerBytes.testFramework.model.dto.ProductListDTO;
+
+public class ProductListDTOTests {
+
+    String urlResource;
+
+    String urlEndpoint;
+
+    String urlQueryParams;
+
+    ProductListDTO productListDTO = new ProductListDTO();
+
+    @BeforeAll
+    void setup(){
+
+    }
+}
---
+++ src/test/java/com/sparta/badgerBytes/tests/UserCredentialsDTOTests.java
@@ -0,0 +1,20 @@
+package com.sparta.badgerBytes.tests;
+
+import org.junit.jupiter.api.BeforeAll;
+import com.sparta.badgerBytes.testFramework.model.dto.UserCredentialsDTO;
+
+public class UserCredentialsDTOTests {
+
+    String urlResource;
+
+    String urlEndpoint;
+
+    String urlQueryParams;
+
+    UserCredentialsDTO userCredentialsDTO = new UserCredentialsDTO();
+
+    @BeforeAll
+    void setup(){
+
+    }
+}
