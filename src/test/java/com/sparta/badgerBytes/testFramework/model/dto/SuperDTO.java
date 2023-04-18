44ca50bdc868a2dd4a7a6ae60ddbaa9dec245709
HamzaKazi <hamzakaziii1@gmail.com>
2023-04-18 12:40:08 +0100

    Added US6 and US5 test cases

--- src/test/java/com/sparta/badgerBytes/testFramework/model/dto/SuperDTO.java
+++ src/test/java/com/sparta/badgerBytes/testFramework/model/dto/SuperDTO.java
@@ -3,10 +3,17 @@ package com.sparta.badgerBytes.testFramework.model.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SuperDTO {
-
     @JsonProperty("responseCode")
-    private String responseCode;
+    private int responseCode;
 
     @JsonProperty("message")
     private String message;
+
+    public int getResponseCodes() {
+        return responseCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/test/java/com/sparta/badgerBytes/tests/ProductListDTOTests.java
+++ src/test/java/com/sparta/badgerBytes/tests/ProductListDTOTests.java
@@ -1,20 +1,72 @@
 package com.sparta.badgerBytes.tests;
 
-import org.junit.jupiter.api.BeforeAll;
 import com.sparta.badgerBytes.testFramework.model.dto.ProductListDTO;
+import com.sparta.badgerBytes.testFramework.controll.ConnectionManager;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.badgerBytes.testFramework.controll.ConnectionManager;
+import com.sparta.badgerBytes.testFramework.model.Injector;
+import com.sparta.badgerBytes.testFramework.model.dto.ProductListDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProductListDTOTests {
 
     String urlResource;
 
-    String urlEndpoint;
+    private static String urlEndpoint = "/searchProduct";
 
     String urlQueryParams;
 
     ProductListDTO productListDTO = new ProductListDTO();
 
     @BeforeAll
-    void setup(){
+    static void setup(){
 
+    }
+
+
+//        private static String urlEndpoint = "/searchProduct";
+
+    @Test
+    @DisplayName("Check a POST request to searchProduct endpoint without search_product parameter returns 400 bad request")
+    void testPostRequestToSearchProductWithoutSearchParam() {
+        ProductListDTO productListDTO = Injector.deserialize(new ProductListDTO(), ConnectionManager.Method.POST, urlEndpoint);
+        Assertions.assertEquals(400, productListDTO.getResponseCodes());
+        Assertions.assertEquals("Bad request, search_product parameter is missing in POST request.", productListDTO.getMessage());
+    }
+
+    @Test
+    @DisplayName("Check a POST request to searchProduct endpoint with an empty search_product parameter returns 400 bad request")
+    void testPostRequestToSearchProductWithEmptySearchParam() {
+        Map<String, String> params = new HashMap<>();
+        params.put("search_product", "");
+        ProductListDTO productListDTO = Injector.deserialize(new ProductListDTO(), ConnectionManager.Method.POST, params, urlEndpoint);
+        Assertions.assertEquals(400, productListDTO.getResponseCodes());
+        Assertions.assertEquals("Bad request, search_product parameter is missing in POST request.", productListDTO.getMessage());
+    }
+
+    @Test
+    @DisplayName("Check a POST request to searchProduct endpoint with a search_product parameter returns 200 OK")
+    void testPostRequestToSearchProductWithSearchParam() {
+        Map<String, String> params = new HashMap<>();
+        params.put("search_product", "tshirt");
+        ProductListDTO productListDTO = Injector.deserialize(new ProductListDTO(), ConnectionManager.Method.POST, params, urlEndpoint);
+        Assertions.assertEquals(200, productListDTO.getResponseCodes());
+        Assertions.assertEquals("OK", productListDTO.getMessage());
+        assertTrue(productListDTO.getProducts().size() > 0, "Expected more than 0 products in the search results");
     }
 }
