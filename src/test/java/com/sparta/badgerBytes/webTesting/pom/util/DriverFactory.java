a9008f7e032d5b8b713e105676c8c494fcaf1967
NicholasDaily <nicholas.william.daily@gmail.com>
2023-04-24 16:04:00 +0100

    Added headless property to the properties file
    Setting it to true will allow you to prevent visible opening of chrome or firefox browsers
    while you run your tests, false will not have that effect :v

--- src/test/java/com/sparta/badgerBytes/webTesting/pom/util/DriverFactory.java
+++ src/test/java/com/sparta/badgerBytes/webTesting/pom/util/DriverFactory.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 import java.io.*;
 import java.util.Properties;
@@ -14,18 +15,26 @@ public class DriverFactory {
     public static WebDriver getDriver() {
         WebDriver driver = null;
         Properties properties = new Properties();
+
         try {
             properties.load(new FileReader(new File("src/test/resources/webTesting.properties")));
+            boolean headless = properties.get("headless").equals("true");
             switch (properties.getProperty("driver")) {
                 case "chrome":
                     ChromeDriverService service = WebAutomationUtil.getChromeDriverService(properties.getProperty("driverPath"));
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("--remote-allow-origins=*");
-                    //options.addArguments("headless");
+                    if(headless) options.addArguments("headless");
                     driver = new ChromeDriver(service, options);
                     break;
                 case "firefox":
                     System.setProperty("webdriver.gecko.driver", properties.getProperty("driverPath"));
+                    if(headless) {
+                        FirefoxOptions ffoptions = new FirefoxOptions();
+                        ffoptions.addArguments("-headless");
+                        driver = new FirefoxDriver(ffoptions);
+                        break;
+                    }
                     driver = new FirefoxDriver();
                     break;
                 case "safari":
